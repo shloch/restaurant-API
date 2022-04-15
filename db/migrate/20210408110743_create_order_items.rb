3eb8613a83c63e7ee00d26bad30b1d52888df73f
shloch <shloch2007@yahoo.fr>
2022-04-15 19:36:12 +0200

    change migrate file

--- db/migrate/20210408110743_create_order_items.rb
+++ db/migrate/20210408110743_create_order_items.rb
@@ -2,7 +2,7 @@ class CreateOrderItems < ActiveRecord::Migration[7.0]
   def change
     create_table :order_items do |t|
       t.references :order, null: false, foreign_key: true
-      t.references :Item, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
 
       t.timestamps
     end
